2ba3d83a2e2a8525260d0696df37550be5c9ca1f
Pichot <gaspard.pichot@strigi-form.fr>
2020-10-13 16:27:25 +0200

    add custom css

--- pages/index.js
+++ pages/index.js
@@ -1,87 +1,23 @@
 import Head from "next/head";
+import { photos } from "../photos";
 
-var sectionStyle = () => {
-  return { backgroundImage: "url('/images/tumbler01.jpg')" };
+var sectionStyle = (url) => {
+  return { backgroundImage: `url('${url}')` };
 };
+
+var photoGalery = photos.map((photo) => (
+  <div className="card" style={sectionStyle(photo.src)}>
+    <div className="description">{photo.author}</div>
+  </div>
+));
+
 const Home = () => (
   <div className="container">
     <main>
-      <div className="photo-grid">
-        <div className="card" style={sectionStyle()}>gtèigyt</div>
-        <div className="card" style={sectionStyle()}></div>
-        <div className="card" style={sectionStyle()}></div>
-        <div className="card" style={sectionStyle()}></div>
-      </div>
+      <div className="photo-grid">{photoGalery}</div>
     </main>
 
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
-    </footer>
-
-    <style jsx>{`
-      @import url("https://fonts.googleapis.com/css?family=Noto+Sans&display=swap");
-
-      body {
-        background: rgb(19, 19, 19);
-        color: #fff;
-        font-family: "Noto Sans", sans-serif;
-      }
-
-      .card {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        background: #353535;
-        font-size: 3rem;
-        color: #fff;
-        box-shadow: rgba(3, 8, 20, 0.1) 0px 0.15rem 0.5rem,
-          rgba(2, 8, 20, 0.1) 0px 0.075rem 0.175rem;
-        height: 100%;
-        width: 100%;
-        border-radius: 4px;
-        transition: all 500ms;
-        overflow: hidden;
-
-        background-size: cover;
-        background-position: center;
-        background-repeat: no-repeat;
-      }
-
-      .card:hover {
-        box-shadow: rgba(2, 8, 20, 0.1) 0px 0.35em 1.175em,
-          rgba(2, 8, 20, 0.08) 0px 0.175em 0.5em;
-        transform: translateY(-3px) scale(1.1);
-      }
-
-      .photo-grid {
-        display: grid;
-        gap: 1rem;
-
-        grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
-        grid-auto-rows: 240px;
-
-        margin-right: 5rem;
-        margin-left: 5rem;
-      }
-
-      /* Medium screens */
-      @media screen and (min-width: 600px) {
-        .card-tall {
-          grid-row: span 2 / auto;
-        }
-
-        .card-wide {
-          grid-column: span 2 / auto;
-        }
-      }
-    `}</style>
+    <footer></footer>
   </div>
 );
 
---
+++ photos.js
@@ -0,0 +1,18 @@
+export const photos = [
+  {
+    src: "/images/tumbler01.jpg",
+    author: "Raphael Zarka",
+  },
+  {
+    src: "/images/tumbler02.jpg",
+    author: "Raphael Zarka",
+  },
+  {
+    src: "/images/tumbler03.jpg",
+    author: "Raphael Zarka",
+  },
+  {
+    src: "/images/tumbler04.jpg",
+    author: "Raphael Zarka",
+  },
+];
