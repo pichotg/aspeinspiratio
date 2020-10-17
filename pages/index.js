5ac4224e450e76bbb602022cd39f0d3b1f0ac461
Pichot Gaspard <pichot.gaspardpro@gmail.com>
2020-10-17 14:20:50 +0200

    get from api

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,31 @@
-import { photos } from "../photos";
-import { useState } from "react";
+// import { photos } from "../photos";
+import { useState, useEffect } from "react";
 import Head from "next/head";
+import getPhotos from "../utils/api";
 
 const Home = () => {
   const shuffle = (arr) => {
     return [...arr].sort(() => Math.random() - 0.5);
   };
-  const [photosList, setPhotos] = useState(shuffle(photos));
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  useEffect(() => {
+    getPhotos().then((data) => {
+      setPhotos(data);
+    });
+  }, []);
+
   return (
     <div className="container">
       <Head>
-        <title>Aspe inspiration</title>
+        <title>Aspeinspiratio</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <header className="navbar">
         <div className="navbar__title navbar__item">Aspe inspiration</div>
         <div
           onClick={() => {
-            setPhotos(shuffle(photosList));
+            setPhotos(shuffle(photos));
           }}
           className="navbar__item"
         >
@@ -29,7 +37,7 @@ const Home = () => {
       </header>
       <div>
         <div className="photo-grid">
-          {photosList.map((photo, idx) => (
+          {photos.map((photo, idx) => (
             <Photo photo={photo} key={idx} />
           ))}
         </div>
---
+++ utils/api.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const TOKEN = "VDToyXHo3EwC1FHxjgnYD3xkxTO2I4RZzRU3fvcVUC72ecqeIL";
+const URL = `https://api.tumblr.com/v2/blog/aspeinspiratio.tumblr.com/posts/photo`;
+
+const photoShape = (w, h) => {
+  if (w > h * 1.48) return "card wide";
+  if (h > w * 1.48) return "card tall";
+  return "card";
+};
+
+export default async function getPhotos() {
+  const res20 = await axios.get(`${URL}?api_key=${TOKEN}`);
+  let tmpList = [];
+  [...res20.data.response.posts].forEach((post) => {
+    let photo = post.photos[0].original_size;
+    tmpList.push({
+      src: photo.url,
+      author: post.summary,
+      shape: photoShape(photo.width, photo.height),
+    });
+    console.log({
+      photo: photo,
+      author: post.summary,
+      shape: photoShape(photo.width, photo.height),
+    });
+  });
+
+  //api get by 20
+  const res40 = await axios.get(`${URL}?api_key=${TOKEN}&offset=20`);
+  [...res40.data.response.posts].forEach((post) => {
+    let photo = post.photos[0].original_size;
+    tmpList.push({
+      src: photo.url,
+      author: post.summary,
+      shape: photoShape(photo.width, photo.height),
+    });
+  });
+
+  return tmpList;
+}
