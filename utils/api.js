9fea7c0d6ae96a7e34af23c1cc2ab340cc4a5296
Pichot Gaspard <pichot.gaspardpro@gmail.com>
2020-10-19 17:31:53 +0200

    change: api fix

--- utils/api.js
+++ utils/api.js
@@ -1,36 +1,39 @@
-import axios from "axios";
-
-const TOKEN = "VDToyXHo3EwC1FHxjgnYD3xkxTO2I4RZzRU3fvcVUC72ecqeIL";
-const URL = `https://api.tumblr.com/v2/blog/aspeinspiratio.tumblr.com/posts/photo`;
+const token = "VDToyXHo3EwC1FHxjgnYD3xkxTO2I4RZzRU3fvcVUC72ecqeIL";
+const url = `https://api.tumblr.com`;
 
 const photoShape = (w, h) => {
-  if (w > h * 1.48) return "card wide";
-  if (h > w * 1.48) return "card tall";
+  if (w > h * 1.5) return "card wide";
+  if (h > w * 1.5) return "card tall";
   return "card";
 };
 
 export default async function getPhotos() {
-  const res20 = await axios.get(`${URL}?api_key=${TOKEN}`);
-  let tmpList = [];
-  [...res20.data.response.posts].forEach((post) => {
-    let photo = post.photos[0].original_size;
-    tmpList.push({
-      src: photo.url,
-      author: post.summary,
-      shape: photoShape(photo.width, photo.height),
+  let PhotosList = [];
+  let offset = 0;
+  while (true) {
+    const response = await fetchPosts(offset);
+    offset += 20;
+    [...response.posts].forEach((post) => {
+      let photo = post.photos[0].original_size;
+      PhotosList.push({
+        src: photo.url,
+        author: post.summary,
+        shape: photoShape(photo.width, photo.height),
+      });
     });
-  });
+    if (PhotosList.length == response.total_posts) break;
+  }
+  return PhotosList;
+}
 
-  //api get by 20
-  const res40 = await axios.get(`${URL}?api_key=${TOKEN}&offset=20`);
-  [...res40.data.response.posts].forEach((post) => {
-    let photo = post.photos[0].original_size;
-    tmpList.push({
-      src: photo.url,
-      author: post.summary,
-      shape: photoShape(photo.width, photo.height),
-    });
-  });
-
-  return tmpList;
+async function fetchPosts(offset) {
+  const res = await fetch(
+    `${url}/v2/blog/aspeinspiratio.tumblr.com/posts/photo?api_key=${token}&offset=${offset}`
+  );
+  if (!res.ok) {
+    const message = `An error has occured: ${res.status}`;
+    throw new Error(message);
+  }
+  const { response } = await res.json();
+  return response;
 }
